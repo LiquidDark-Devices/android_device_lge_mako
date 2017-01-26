a11321c4fdd5167aaa9b5b1358076fe530f75dcb
nitin1438 <nitin.chobhe@gmail.com>
2017-01-26 23:24:59 +0530

    Liquidify mako

--- liquid.mk
+++ liquid.mk
@@ -3,17 +3,21 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := lineage_mako
+PRODUCT_NAME := liquid_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
+
+#Ship Cam 2
+PRODUCT_PACKAGES += \
+    Camera2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
